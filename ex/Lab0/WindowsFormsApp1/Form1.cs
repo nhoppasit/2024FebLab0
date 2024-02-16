91ee540ef4ba12bb37afccc00774dfc88f50227e
Nhoppasit S <nhoppasit@gmail.com>
2024-02-16 11:09:54 +0700

    Add a ex proj

---
+++ ex/Lab0/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void btnConnect_KeyUp(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Hi");
+
+        }
+    }
+}
